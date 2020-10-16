3d9fc4b55f0a0cf2db65cfd3268b1d9ffff10df4
saad khan <saadgamer124@gmail.com>
2020-10-16 21:13:17 +0530

    Created the Shop Page and added the styling for it

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from "react";
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
-function HatsPage() {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-}
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
